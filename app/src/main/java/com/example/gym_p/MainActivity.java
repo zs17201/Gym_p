0d13502e963b692390a6377837674dbdc147e66a
zs17201 <zs17201@gmail.com>
2024-12-23 16:16:37 +0200

    shabat shalom

--- app/src/main/java/com/example/gym_p/MainActivity.java
+++ app/src/main/java/com/example/gym_p/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //good shabas
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
