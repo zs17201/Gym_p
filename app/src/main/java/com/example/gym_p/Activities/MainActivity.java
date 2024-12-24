178cb9ca13ba0ed1ac7f55ca51cc9d21ba693c35
zs17201 <zs17201@gmail.com>
2024-12-24 14:53:41 +0200

    connect firebase

--- app/src/main/java/com/example/gym_p/Activities/MainActivity.java
+++ app/src/main/java/com/example/gym_p/Activities/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.gym_p.Activities;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.Navigation;
 
 import com.example.gym_p.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    //good shabas
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +29,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+
+    public void reg(String email,String pass){
+
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show();
+                       // Navigation.findNavController().navigate(R.id.action_signupFragment_to_logInFragment);
+                    } else {
+                        Toast.makeText(this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
 }
